8da22af17305b0697d017d7d323b9e9427c161f7
Lokesh Illa <li21409@corp.ojas-it.com>
2022-06-14 12:04:52 +0530

    ok

---
+++ src/main/java/com/ojas/Calculator.java
@@ -0,0 +1,26 @@
+package com.ojas;
+
+public class Calculator {
+
+	public Integer add(Integer a, Integer b) {
+		return a + b;
+	}
+
+	public Integer mul(Integer a, Integer b) {
+		return a * b;
+	}
+
+	public Integer div(Integer a, Integer b) {
+		try {
+			Thread.sleep(1005);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return a / b;
+	}
+	
+	public Integer sub(Integer a,Integer c)
+	{
+		return 24;
+	}
+}
